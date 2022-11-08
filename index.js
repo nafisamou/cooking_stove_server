8425a900c30c088e674a30a82624a61c59a317c3
nafisamou <108416863+nafisamou@users.noreply.github.com>
2022-11-09 03:01:51 +0600

    colors removed

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-require("dotenv").config();
-const colors = require("colors");
-colors.setTheme({
-  silly: "rainbow",
-  input: "grey",
-  verbose: "cyan",
-  prompt: "grey",
-  info: "green",
-  data: "grey",
-  help: "cyan",
-  warn: "yellow",
-  debug: "blue",
-  error: "red",
-});
 
 // middle wares:
 app.use(cors());
 app.use(express.json());
+
+// const colors = require("colors");
+// colors.setTheme({
+//   silly: "rainbow",
+//   input: "grey",
+//   verbose: "cyan",
+//   prompt: "grey",
+//   info: "green",
+//   data: "grey",
+//   help: "cyan",
+//   warn: "yellow",
+//   debug: "blue",
+//   error: "red",
+// });
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.grwbbr0.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
@@ -35,17 +37,21 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     const serviceCollection = client.db("cloudKitchen").collection("services");
-    const orderCollection = client.db("cloudKitchen").collection("orders");
+    // const orderCollection = client.db("cloudKitchen").collection("orders");
 
     app.get("/services", async (req, res) => {
       const query = {};
       const cursor = serviceCollection.find(query);
-      const services = await cursor.toArray();
-      res.send({
-        success: true,
-        message: "Successfully got the data",
-        data: services});
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
     });
+    app.get("/servicesAll", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
@@ -53,7 +59,7 @@ async function run() {
       res.send(service);
     });
   }  catch (error) {
-    console.log(error.name.bgRed, error.message.bold);
+    console.log(error);
     res.send({
       success: false,
       error: error.message,
@@ -73,7 +79,7 @@ app.listen(port, () => {
     if (err) {
       console.log(err);
     } else {
-      console.log(colors.yellow("Connected to MongoDB"));
+      console.log(("Connected to MongoDB"));
     }
   });
   console.log(`Cloud Kitchen server is running on ${port}`);
